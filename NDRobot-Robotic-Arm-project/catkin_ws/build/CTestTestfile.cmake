06274877872bf1ecf3616c688e2769d4ff4da75c
yacin <yacin.ha9@gmail.com>
2023-12-11 14:56:39 +0100

    robotic arm initial files

---
+++ NDRobot-Robotic-Arm-project/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/catkin_ws/src
+# Build directory: /home/robond/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(RoboND-Kinematics-Project/gazebo_grasp_plugin)
+subdirs(RoboND-Kinematics-Project/kuka_arm)
+subdirs(RoboND-Kinematics-Project/kr210_claw_moveit)

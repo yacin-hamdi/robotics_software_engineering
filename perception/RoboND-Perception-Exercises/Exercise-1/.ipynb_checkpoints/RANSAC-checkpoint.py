98adb507ca7eccdbe01cc1a12b2466695fe25d07
yacin <yacin.ha9@gmail.com>
2023-12-29 14:08:27 +0100

    add pass through filter

--- perception/RoboND-Perception-Exercises/Exercise-1/.ipynb_checkpoints/RANSAC-checkpoint.py
+++ perception/RoboND-Perception-Exercises/Exercise-1/.ipynb_checkpoints/RANSAC-checkpoint.py
@@ -7,13 +7,23 @@ cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 # Voxel Grid filter
 vox = cloud.make_voxel_grid_filter()
-LEAF_SIZE = 0.01
 
+LEAF_SIZE = 0.01
 vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
 cloud_filtered = vox.filter()
 
 # PassThrough filter
+passthrough = cloud_filtered.make_passthrough_filter()
 
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
+
+cloud_filtered = passthrough.filter()
 
 # RANSAC plane segmentation
 

da857265cfc655e55906c71e610f4d60919f6870
yacin <yacin.ha9@gmail.com>
2024-02-14 16:46:11 +0100

    implement the state prediction step of the 1d kalman filter

--- kalman_filter/measurement_update.cpp
+++ kalman_filter/measurement_update.cpp
@@ -11,8 +11,14 @@ tuple<double, double> measurement_update(double mean1, double var1, double mean2
 	return make_tuple(new_mean, new_var);
 }
 
+tuple<double, double> state_prediction(double mean1, double var1, double mean2, double var2) {
+	new_mean = mean1 + mean2;
+	new_var = var1 + var2;
+	return make_tuple(new_mean, new_var);
+}
+
 int main() {
-	tie(new_mean, new_var) = measurement_update(20, 9, 30, 3);
+	tie(new_mean, new_var) = state_prediction(10, 4, 12, 4);
 	printf("[%f, %f]", new_mean, new_var);
 	return 0;
 }
